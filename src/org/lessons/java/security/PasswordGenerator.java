523b3e4ce4da40afdf200836a5f66aa1ee6cf07c
Domiziano De Santis <119798750+DeSantisDomiziano@users.noreply.github.com>
2023-09-13 15:56:11 +0200

    add gen pw

--- src/org/lessons/java/security/PasswordGenerator.java
+++ src/org/lessons/java/security/PasswordGenerator.java
@@ -1,5 +1,40 @@
 package org.lessons.java.security;
 
-public class PasswordGenerator {
+import java.util.Scanner;
 
+public class PasswordGenerator {
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+
+		//PRENDO I DATI CHE MI SERVONO
+		System.out.print("crea ora la tua password poco sicura, cominciamo: \nscrivi qui il tuo nome: ");
+		String nome = sc.nextLine();
+		
+		System.out.print("scrivi qui il tuo cognome: ");
+		String cognome = sc.nextLine();
+		
+		System.out.print("scrivi qui il tuo colore preferito: ");
+		String favColor = sc.nextLine();
+		
+		System.out.print("scrivi qui il giorno di nascita: ");
+		short giorno = sc.nextShort();
+		
+		System.out.print("scrivi qui il mese di nascita: ");
+		short mese = sc.nextShort();
+		
+		System.out.print("scrivi qui il anno di nascita: ");
+		short anno = sc.nextShort();
+		
+		//SOMMO LA DATA
+		int sumAgeOfBirth = giorno + mese + anno;
+		
+		//RESTITUISCO LA PW
+		System.out.println("La tua password è: " + nome + "-" + cognome + "-" + favColor + "-" + sumAgeOfBirth);
+		
+		//CHIUDO LO SCANNER
+		sc.close();
+
+
+	}
 }
